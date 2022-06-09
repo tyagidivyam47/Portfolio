a8ab7db6f095a56b4802ca921a57da9682ec4dd8
Divyam Tyagi <tyagidivyam47@gmail.com>
2022-06-09 22:06:05 +0530

    Added scroll to view work

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
@@ -19,12 +20,14 @@ const Home = () => {
           building responsive full-stack web applications.
         </p>
         <div>
-          <button className="text-white group border-2 border-orange-500 px-6 py-3 my-2 flex items-center hover:bg-orange-500 hover:border-orange-500 transition-all" >
-            View Work
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-3 " />
-            </span>
-          </button>
+          <Link to="work" smooth={true} duration={500}>
+            <button className="text-white group border-2 border-orange-500 px-6 py-3 my-2 flex items-center hover:bg-orange-500 hover:border-orange-500 transition-all">
+              View Work
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3 " />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
