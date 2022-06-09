1d2de7196c09254802d03587880d8d9409ba987a
Divyam Tyagi <tyagidivyam47@gmail.com>
2022-06-09 22:14:51 +0530

    Changed about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,11 +18,10 @@ const About = () => {
           </div>
           <div>
             <p>
-              I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?
+              I am passionate about building excellent webpages that improves
+              the lives of those around me. I am a Computer Science Student. I
+              made my hands dirty in C++, JAVA, JAVA DSA and Android. Now going
+              to focus on Full Stack domain.
             </p>
           </div>
         </div>
